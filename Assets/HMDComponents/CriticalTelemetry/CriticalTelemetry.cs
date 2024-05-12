f395efa5ca8c6aed4ca56cef63064ea12fa0e3c3
areich128 <alex.reich128@gmail.com>
2024-05-12 11:39:08 -0700

    more main scene changes plus added that it turns red if under off-nominal conditions

--- Assets/HMDComponents/CriticalTelemetry/CriticalTelemetry.cs
+++ Assets/HMDComponents/CriticalTelemetry/CriticalTelemetry.cs
@@ -23,58 +23,6 @@ public class CriticalTelemetry : MonoBehaviour
         oxyTime.gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    // void Update()
-    // {
-    // 
-    //     /// CHECK/UPDATE TODOLIST 
-    //     if (connection != null)
-    //     {
-    // 
-    //         GatewayConnection conn = connection.GetConnection();
-    // 
-    //         if (conn != null)
-    //         {
-    //             string telem = conn.GetTELEMETRYJsonString();
-    //             JObject jo = JObject.Parse(telem);
-    // 
-    //             bpmText.text = "BPM: ";
-    // 
-    //             float bpm1 = jo["telemetry"]["eva1"]["heart_rate"].ToObject<float>();
-    //             bpmText.text += bpm1;
-    // 
-    //             if (jo["telemetry"]["eva1"]["heart_rate"].ToObject<float>() > 100) //change to off-nominal values
-    //             {
-    //                 bpmText.color = Color.red;
-    //             }
-    //             else
-    //             {
-    //                 bpmText.color = Color.green;
-    //             }
-    // 
-    //             battLife.text = "Remaining Battery Life: ";
-    //             float batlife = jo["telemetry"]["eva1"]["batt_time_left"].ToObject<float>();
-    //             int bhours = (int)batlife / 3600;
-    //             batlife %= 3600;
-    //             int bminutes = (int)batlife / 60;
-    //             batlife %= 60;
-    // 
-    //             battLife.text += $"{bhours}:{bminutes}:{(int)batlife}";
-    // 
-    //             oxyTime.text = "Remaining Oxygen Time: ";
-    //             float oxytime = jo["telemetry"]["eva1"]["oxy_time_left"].ToObject<float>();
-    // 
-    //             int ohours = (int)oxytime / 3600;
-    //             oxytime %= 3600;
-    //             int ominutes = (int)oxytime / 60;
-    //             oxytime %= 60;
-    // 
-    //             oxyTime.text += $"{ohours}:{ominutes}:{(int)oxytime}";
-    //         }
-    //     }
-    // 
-    // }
-
     void Update()
     {
         if (connection == null)
@@ -117,25 +65,31 @@ public class CriticalTelemetry : MonoBehaviour
         float bpm1 = jo["telemetry"]["eva1"]["heart_rate"].ToObject<float>();
         bpmText.text += bpm1;
 
-        bpmText.color = bpm1 > 100 ? Color.red : Color.green;
+        bpmText.color = (bpm1 > 160 || bpm1 < 50) ? Color.red : Color.green;
     }
 
     void UpdateBatteryLife(JObject jo)
     {
         battLife.text = "Remaining Battery Life: ";
         float batlife = jo["telemetry"]["eva1"]["batt_time_left"].ToObject<float>();
+
+        battLife.color = batlife < 3600 ? Color.red : Color.green;
+
         int bhours = (int)batlife / 3600;
         batlife %= 3600;
         int bminutes = (int)batlife / 60;
         batlife %= 60;
 
         battLife.text += $"{bhours:D2}:{bminutes:D2}:{(int)batlife:D2}";
+
     }
 
     void UpdateOxygenTime(JObject jo)
     {
         oxyTime.text = "Remaining Oxygen Time: ";
         float oxytime = jo["telemetry"]["eva1"]["oxy_time_left"].ToObject<float>();
+
+        oxyTime.color = oxytime < 3600 ? Color.red : Color.green;
 
         int ohours = (int)oxytime / 3600;
         oxytime %= 3600;
