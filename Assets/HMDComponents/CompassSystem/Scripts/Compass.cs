ed45abbbb0938676a3552a29f7d9f26f42c4bc85
IRawal <52429323+IRawal@users.noreply.github.com>
2024-05-20 00:54:25 -0700

    Compass

--- Assets/HMDComponents/CompassSystem/Scripts/Compass.cs
+++ Assets/HMDComponents/CompassSystem/Scripts/Compass.cs
@@ -9,32 +9,39 @@ public class Compass : MonoBehaviour
 
     // Start is called before the first frame update
     public ConnectionHandler connectionHandler;
+    private GatewayConnection conn;
+
+    float lastTime;
+    float cameraOffset;
+
     void Start()
     {
-        
+        conn = connectionHandler.GetConnection();
+        lastTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
         float cameraYaw = Camera.main.transform.rotation.eulerAngles.y;
-        float cameraOffset = 0;
 
-        GatewayConnection conn = connectionHandler.GetConnection();
-        if (conn != null && conn.isIMUUpdated())
+        if (Time.time - lastTime > 1) 
         {
-
+            lastTime = Time.time;
             string IMUstring = conn.GetIMUJsonString();
 
             // Load IMU data into map
             JObject jo = JObject.Parse(IMUstring);
             float tssHeading = jo["imu"]["eva1"]["heading"].ToObject<float>();
-
             cameraOffset = tssHeading - cameraYaw;
+            //Debug.Log(cameraOffset);
         }
-        
+
+
         // Update with tss server/prevent desync
         cameraYaw += cameraOffset;
+
+        //Debug.Log(cameraYaw);
 
         // Rotate image
         CompassImage.uvRect = new Rect(cameraYaw / 360, 0, 1, 1);
