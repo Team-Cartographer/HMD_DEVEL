a6c25428e42c71fb639e0a6f39dbc4fb09830b60
areich128 <alex.reich128@gmail.com>
2024-05-19 18:11:37 -0700

    voice commands and hard procedures

--- Assets/HMDComponents/HardProcedures/HardProcedures2.cs
+++ Assets/HMDComponents/HardProcedures/HardProcedures2.cs
@@ -1,7 +1,15 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Hardware;
 using UnityEngine;
+using UnityEngine.InputSystem.HID;
+using UnityEngine.InputSystem;
+using UnityEngine.SocialPlatforms;
 using UnityEngine.UI;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using static UnityEditor.PlayerSettings;
 
 public class HardProcedures2 : MonoBehaviour
 {
@@ -47,12 +55,19 @@ public class HardProcedures2 : MonoBehaviour
 
     void InitializeProcedures()
     {
-        procedures.Add("EGRESS", new List<string> { "Egress step 1", "Egress step 2" });
+        procedures.Add("EGRESS", new List<string> {"1. EV1 and EV2 connect UIA and DCU umbilical",
+            "2. EV-1, EV-2 PWR – ON", 
+            "3. BATT – UMB", 
+            "4. DEPRESS PUMP PWR – ON", 
+            "4. OXYGEN O2 VENT – OPEN", 
+            "5. Wait until both Primary and Secondary OXY tanks are < 10psi UIA 3. OXYGEN O2 VENT – CLOSE BOTH DCU 4. OXY – PRI UIA 5. OXYGEN EMU-1, EMU-2 – OPEN HMD 6. Wait until EV1 and EV2 Primary O2 tanks > 3000 psi UIA 7. OXYGEN EMU-1, EMU-2 – CLOSE BOTH DCU 8. OXY – SEC UIA 9. OXYGEN EMU-1, EMU-2 – OPEN HMD 10. Wait until EV1 and EV2 Secondary O2 tanks > 3000 psi UIA 11. OXYGEN EMU-1, EMU-2 – CLOSE BOTH DCU 12. OXY – PRI Prep Water Tanks BOTH DCU 1. PUMP – OPEN UIA 2. EV-1, EV-2 WASTE WATER – OPEN HMD 3. Wait until water EV1 and EV2 Coolant tank is < 5% UIA 4. EV-1, EV-2 WASTE WATER – CLOSE UIA 5. EV-1, EV-2 SUPPLY WATER – OPEN HMD 6. Wait until water EV1 and EV2 Coolant tank is > 95% UIA 7. EV-1, EV-2 SUPPLY WATER – CLOSE BOTH DCU 8. PUMP – CLOSE END Depress, Check Switches and Disconnect HMD 1. Wait until SUIT P, O2 P = 4 UIA 2. DEPRESS PUMP PWR – OFF BOTH DCU 3. BATT – LOCAL UIA 9. EV-1, EV-2 PWR - OFF BOTH DCU 4. Verify OXY – PRI BOTH DCU 5. Verify COMMS – A BOTH DCU 6. Verify FAN – PRI BOTH DCU 7. Verify PUMP – CLOSE BOTH DCU 8. Verify CO2 – AUIA and DCU 9. EV1 and EV2 disconnect UIA and DCU umbilica" });
+
         procedures.Add("GEO", new List<string> { "1. If available, perform Field Notes on Rock, which can include picture, voice notes, etc.",
                                                 "2. Press and HOLD XRF trigger",
                                                 "3. Aim close to sample until beep, then release trigger",
                                                 "4. If Rock Composition outside of nominal parameters, collect rock",
                                                 "5. If able, drop and label a pin" });
+
         procedures.Add("REPAIR", new List<string> { "Repair step 1", "Repair step 2" });
         procedures.Add("INGRESS", new List<string> { "Ingress step 1", "Ingress step 2" });
     }
